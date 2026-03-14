e14d3aff5d542ab2e65d95ccc84e07936f23a07b
Diana Ahmadi <da223is@student.lnu.se>
2026-03-14 22:54:07 +0100

    Almost end in U2.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let index=0;
 const leftBtn= document.getElementById("leftBtn")
 const rightBtn= document.getElementById("rightBtn")
 
+if (rightBtn&&pizzas.length>0){
 rightBtn.onclick= function(){
     pizzas[index].classList.remove("active")
     index++
@@ -19,5 +20,13 @@ leftBtn.onclick= function(){
         index= pizzas.length-1
     }
     pizzas[index].classList.add("active")
-}
+}}
+const currentPage= window.location.pathname.split("/").pop();
+const links= document.querySelectorAll("nav a")
 
+links.forEach(link=> {
+    const linkPages= link.getAttribute("href").split("/").pop();
+    if(linkPages=== currentPage){
+        link.classList.add("active")
+    }
+});
